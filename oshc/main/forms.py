df1ea0ee5f89231211f0475b4f5f592695a30ee5
Alexandre Pinto <apinto@ubiwhere.com>
2017-10-23 17:30:55 +0100

    Use crispy forms in contests

---
+++ oshc/main/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from crispy_forms.helper import FormHelper
+from .models import Contest
+
+
+class ContestForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(ContestForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_tag = False
+        self.helper.label_class = 'col-lg-2'
+        self.helper.field_class = 'col-lg-8'
+
+    class Meta:
+        model = Contest
+        fields = ['name', 'link', 'description', 'start_date', 'end_date']
